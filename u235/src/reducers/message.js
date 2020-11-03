36cfda0b3d91e5315b3e0a8bb46105381947c9dc
Hlumisa Mazomba <hmazomba@live.co.za>
2020-11-03 21:15:09 +0200

    Create redux reducers
    The reducers will update different parts of the state which correspond to dispatched redux actions

---
+++ u235/src/reducers/message.js
@@ -0,0 +1,19 @@
+//This reducer updates the message state when the message action is dispatch from anywhere in the app
+import {SET_MESSAGE, CLEAR_MESSAGE } from "../actions/types";
+
+const initialState = {};
+
+export default function (state = initialState, action){
+    const{type, payload} = action;
+
+    switch(type){
+        case  SET_MESSAGE:
+            return { message: payload };
+        
+        case CLEAR_MESSAGE:
+            return{ message: ""};
+            
+        default: 
+            return state;    
+    }
+}
