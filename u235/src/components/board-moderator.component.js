3d549fa5c0e189cd621c4a6ea24083ba990b6675
Hlumisa Mazomba <hmazomba@live.co.za>
2020-11-04 00:49:28 +0200

    Resource Components
    These components will use the User service to request data from the API.

---
+++ u235/src/components/board-moderator.component.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import UserService from "../services/user.service";
+
+export default class Home extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            content: ""
+        };
+    }
+
+    componentDidMount() {
+        UserService.getModeratorBoard().then(
+            response => {
+                this.setState({
+                    content: response.data
+                });
+            },
+            error => {
+                this.setState({
+                    content:
+                    (error.response && error.response.data && serror.response.data.message ||
+                        error.message ||
+                        error.toString())
+                });
+            }
+        );
+    }
+    
+    render() {
+        return(
+            <div className="container">
+                <header className="jumbotron"></header>
+            </div>
+        );
+    }
+}
